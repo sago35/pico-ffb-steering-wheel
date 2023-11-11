a9e14aca4b901195be1007f38e3d4fea845f49ea
sago35 <sago35@gmail.com>
2023-11-11 15:00:20 +0900

    merge upstream

--- pid/pid_handler.go
+++ pid/pid_handler.go
@@ -1,6 +1,7 @@
 package pid
 
 import (
+	"diy-ffb-wheel/logger"
 	"fmt"
 	"machine"
 	"machine/usb"
@@ -9,23 +10,31 @@ import (
 )
 
 type PIDHandler struct {
-	effectStates [MAX_EFFECTS]*TEffectState
+	effectStates []*TEffectState
+	pidBlockLoad PIDBlockLoadFeatureData
+	pidPool      PIDPoolFeatureData
 	gains        Gains
 	params       EffectParams
 	nextEID      uint8
 	enabled      bool
 	paused       bool
 	gain         uint8
-	pidBlockLoad PIDBlockLoadFeatureData
 }
 
 func NewPIDHandler() *PIDHandler {
-	effects := [MAX_EFFECTS]*TEffectState{}
+	effects := make([]*TEffectState, MAX_EFFECTS)
 	for i := range effects[:] {
 		effects[i] = &TEffectState{}
 	}
 	return &PIDHandler{
 		effectStates: effects,
+		pidBlockLoad: PIDBlockLoadFeatureData{b: make([]byte, 5)},
+		pidPool: PIDPoolFeatureData{
+			ReportID:               7,
+			RamPoolSize:            MEMORY_SIZE,
+			MaxSimultaneousEffects: MAX_EFFECTS,
+			MemoryManagement:       3,
+			b:                      make([]byte, 5)},
 		gains: Gains{
 			TotalGain:    255,
 			ConstantGain: 255,
@@ -105,12 +114,7 @@ func (m *PIDHandler) GetReport(setup usb.Setup) bool {
 			machine.SendUSBInPacket(0, b)
 			return true
 		case 7:
-			b, _ := PIDPoolFeatureData{
-				ReportID:               7,
-				RamPoolSize:            MEMORY_SIZE,
-				MaxSimultaneousEffects: MAX_EFFECTS,
-				MemoryManagement:       3,
-			}.MarshalBinary()
+			b, _ := m.pidPool.MarshalBinary()
 			machine.SendUSBInPacket(0, b)
 			return true
 		}
@@ -119,12 +123,12 @@ func (m *PIDHandler) GetReport(setup usb.Setup) bool {
 }
 
 func (m *PIDHandler) GetIdle(setup usb.Setup) bool {
-	machine.SendUSBInPacket(0, []byte{0})
+	machine.SendZlp()
 	return true
 }
 
 func (m *PIDHandler) GetProtocol(setup usb.Setup) bool {
-	machine.SendUSBInPacket(0, []byte{0})
+	machine.SendZlp()
 	return true
 }
 
@@ -242,7 +246,7 @@ func (m *PIDHandler) StopEffect(id uint8) {
 func (m *PIDHandler) FreeAllEffects() {
 	m.nextEID = 1
 	for id := uint8(0); id < MAX_EFFECTS; id++ {
-		*m.effectStates[id] = TEffectState{}
+		m.effectStates[id].Clear()
 	}
 	m.pidBlockLoad.RamPoolAvailable = MEMORY_SIZE
 }
@@ -261,6 +265,7 @@ func (m *PIDHandler) FreeEffect(id uint8) {
 
 // SetEffect reportId == 0x01
 func (m *PIDHandler) SetEffect(b []byte) {
+	logger.Debugln("SetEffect:", b)
 	var v SetEffectOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -274,6 +279,7 @@ func (m *PIDHandler) SetEffect(b []byte) {
 
 // SetEnvelope reportId == 0x02
 func (m *PIDHandler) SetEnvelope(b []byte) {
+	logger.Debugln("SetEnvelope:", b)
 	var v SetEnvelopeOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -285,6 +291,7 @@ func (m *PIDHandler) SetEnvelope(b []byte) {
 
 // SetCondition reportId == 0x03
 func (m *PIDHandler) SetCondition(b []byte) {
+	logger.Debugln("SetCondition:", b)
 	var v SetConditionOutputData
 	_ = v.UnmarshalBinary(b)
 	axis := v.ParameterBlockOffset
@@ -304,6 +311,7 @@ func (m *PIDHandler) SetCondition(b []byte) {
 
 // SetPeriodic reportId == 0x04
 func (m *PIDHandler) SetPeriodic(b []byte) {
+	logger.Debugln("SetPeriodic:", b)
 	var v SetPeriodicOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -315,6 +323,7 @@ func (m *PIDHandler) SetPeriodic(b []byte) {
 
 // SetConstantForce reportId == 0x05
 func (m *PIDHandler) SetConstantForce(b []byte) {
+	logger.Debugln("SetConstantForce:", b)
 	var v SetConstantForceOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -323,6 +332,7 @@ func (m *PIDHandler) SetConstantForce(b []byte) {
 
 // SetRampForce reportId == 0x06
 func (m *PIDHandler) SetRampForce(b []byte) {
+	logger.Debugln("SetRampForce:", b)
 	var v SetRampForceOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -332,6 +342,7 @@ func (m *PIDHandler) SetRampForce(b []byte) {
 
 // SetCustomForceData reportId == 0x07
 func (m *PIDHandler) SetCustomForceData(b []byte) {
+	logger.Debugln("SetCustomForceData:", b)
 	var v SetCustomForceDataOutputData
 	_ = v.UnmarshalBinary(b)
 	// TODO: implement
@@ -339,6 +350,7 @@ func (m *PIDHandler) SetCustomForceData(b []byte) {
 
 // SetDownloadForceSample reportId == 0x08
 func (m *PIDHandler) SetDownloadForceSample(b []byte) {
+	logger.Debugln("SetDownloadForceSample:", b)
 	var v SetDownloadForceSampleOutputData
 	_ = v.UnmarshalBinary(b)
 	// TODO: implement
@@ -346,6 +358,7 @@ func (m *PIDHandler) SetDownloadForceSample(b []byte) {
 
 // EffectOperation reportId == 0x0a
 func (m *PIDHandler) EffectOperation(b []byte) {
+	logger.Debugln("EffectOperation:", b)
 	var v EffectOperationOutputData
 	_ = v.UnmarshalBinary(b)
 	switch v.Operation {
@@ -357,7 +370,6 @@ func (m *PIDHandler) EffectOperation(b []byte) {
 		default:
 			effect.Duration *= uint16(v.LoopCount)
 		}
-		m.effectStates[v.EffectBlockIndex] = effect
 		m.StartEffect(v.EffectBlockIndex)
 	case EOStartSolo:
 		m.StopAllEffects()
@@ -369,6 +381,7 @@ func (m *PIDHandler) EffectOperation(b []byte) {
 
 // BlockFree reportId == 0x0b
 func (m *PIDHandler) BlockFree(b []byte) {
+	logger.Debugln("BlockFree:", b)
 	var v BlockFreeOutputData
 	_ = v.UnmarshalBinary(b)
 	if v.EffectBlockIndex == 0xff {
@@ -380,6 +393,7 @@ func (m *PIDHandler) BlockFree(b []byte) {
 
 // DeviceControl reportId == 0x0c
 func (m *PIDHandler) DeviceControl(b []byte) {
+	logger.Debugln("DeviceControl:", b)
 	var v DeviceControlOutputData
 	_ = v.UnmarshalBinary(b)
 	switch v.Control {
@@ -400,6 +414,7 @@ func (m *PIDHandler) DeviceControl(b []byte) {
 
 // DeviceGain reportId == 0x0d
 func (m *PIDHandler) DeviceGain(b []byte) {
+	logger.Debugln("DeviceGain:", b)
 	var v DeviceGainOutputData
 	_ = v.UnmarshalBinary(b)
 	m.gain = v.Gain
@@ -407,6 +422,7 @@ func (m *PIDHandler) DeviceGain(b []byte) {
 
 // SetCustomForce reportId == 0x0e
 func (m *PIDHandler) SetCustomForce(b []byte) {
+	logger.Debugln("SetCustomForce:", b)
 	var v SetCustomForceOutputData
 	_ = v.UnmarshalBinary(b)
 	// TODO: implement
